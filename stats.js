9e2545bad8eb5b6ab06a1a31c772cc3e59d83ddd
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2021-12-15 15:41:40 -0400

    Stats are only added if relevant

--- stats.js
+++ stats.js
@@ -5,30 +5,44 @@ export async function main(ns) {
     const doc = eval('document');
     const hook0 = doc.getElementById('overview-extra-hook-0');
     const hook1 = doc.getElementById('overview-extra-hook-1');
-    const stkSymbols = await getNsDataThroughFile(ns, `ns.stock.getSymbols()`, '/Temp/stock-symbols.txt');
+    let stkSymbols = null;
+    try { stkSymbols = await getNsDataThroughFile(ns, `ns.stock.getSymbols()`, '/Temp/stock-symbols.txt'); } catch { }
     while (true) {
         try {
-            const headers = ["Hashes", "Stock", "ScrInc", "ScrExp"]
+            const headers = []
             const values = [];
+
             const hashes = await getNsDataThroughFile(ns, '[ns.hacknet.numHashes(), ns.hacknet.hashCapacity()]', '/Temp/hash-stats.txt')
-            values.push(`${formatNumberShort(hashes[0], 3, 1)}/${formatNumberShort(hashes[1], 3, 1)}`);
+            if (hashes[1] > 0) {
+                headers.push("Hashes");
+                values.push(`${formatNumberShort(hashes[0], 3, 1)}/${formatNumberShort(hashes[1], 3, 1)}`);
+            }
             // Detect and notify the HUD if we are liquidating
             if (ns.ps("home").some(p => p.filename.includes('spend-hacknet-hashes') && (p.args.includes("--liquidate") || p.args.includes("-l")))) {
                 headers.splice(1, 0, " ");
                 values.push("Liquidating");
             }
-            const stkPortfolio = await getNsDataThroughFile(ns, JSON.stringify(stkSymbols) +
-                `.map(sym => ({ sym, pos: ns.stock.getPosition(sym), ask: ns.stock.getAskPrice(sym), bid: ns.stock.getBidPrice(sym) }))` +
-                `.reduce((total, stk) => total + stk.pos[0] * stk.bid + stk.pos[2] * (stk.ask * 2 - stk.bid), 0)`,
-                '/Temp/stock-portfolio-value.txt')
-            values.push(formatMoney(stkPortfolio));
+
+            if (stkSymbols) {
+                headers.push("Stock");
+                const stkPortfolio = await getNsDataThroughFile(ns, JSON.stringify(stkSymbols) +
+                    `.map(sym => ({ sym, pos: ns.stock.getPosition(sym), ask: ns.stock.getAskPrice(sym), bid: ns.stock.getBidPrice(sym) }))` +
+                    `.reduce((total, stk) => total + stk.pos[0] * stk.bid + stk.pos[2] * (stk.ask * 2 - stk.bid), 0)`,
+                    '/Temp/stock-portfolio-value.txt')
+                values.push(formatMoney(stkPortfolio));
+            }
+            headers.push("ScrInc");
             values.push(formatMoney(ns.getScriptIncome()[0], 3, 2) + '/sec');
+
+            headers.push("ScrExp");
             values.push(formatNumberShort(ns.getScriptExpGain(), 3, 2) + '/sec');
+
             const gangInfo = await getNsDataThroughFile(ns, 'ns.gang.inGang() ? ns.gang.getGangInformation() : false', '/Temp/gang-stats.txt');
             if (gangInfo !== false) {
                 headers.push("Gang");
                 values.push(formatMoney(gangInfo.moneyGainRate * 5, 3, 2) + '/sec');
             }
+
             const karma = ns.heart.break();
             if (karma < -100) {
                 headers.push("Karma");
