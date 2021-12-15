748874a302825e8fadd6565f7019a0561037ab85
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2021-12-15 17:03:41 -0400

    Some improvements to git pul

--- git-pull.js
+++ git-pull.js
@@ -4,6 +4,7 @@ const argsSchema = [
     ['branch', 'main'],
     ['download', []], // By default, all files returned by ns.ls() will be downloaded. Override with just a subset of files here
     ['new-file', []], // By default, only files returned by ns.ls() will be downloaded. You can add additional files to seek out here.
+    ['subfolder', ''], // Can be set to download to a sub-folder that is not part of the remote repository structure
 ];
 
 export function autocomplete(data, _) {
@@ -16,17 +17,18 @@ export function autocomplete(data, _) {
  * You are responsible for:
  * - Backing up your save / scripts first (try `download *` in the terminal)
  * - Ensuring you have no local changes that you don't mind getting overwritten
- * - Ensuring scripts are stopped so that the attempt to overwrite them doesn't fail.
  * TODO: Some way to list all files in the repository and/or download them all. **/
 export async function main(ns) {
     const options = ns.flags(argsSchema);
-    const baseUrl = `https://raw.githubusercontent.com/${options.github}/${options.repository}/${options.branch}/`
-    const filesToDownload = (options.download.length > 0 ? options.download : ns.ls('home')).concat(options['new-file'])
-    for (const filename of filesToDownload) {
-        ns.print(`Trying to download ${baseUrl}${filename}...`)
-        if (await ns.wget(`${baseUrl}${filename}?ts=${new Date().getTime()}`, filename))
-            ns.tprint(`SUCCESS: Updated ${filename} to the latest from ${baseUrl}${filename}`)
+    const baseUrl = `https://raw.githubusercontent.com/${options.github}/${options.repository}/${options.branch}/`;
+    const filesToDownload = options['new-file'].concat(options.download.length > 0 ? options.download : ns.ls('home')
+        .filter(name => !name.endsWith(".exe") && !name.endsWith(".msg") && !name.endsWith(".lit")));
+    for (const localFilePath of filesToDownload) {
+        const remoteFilePath = baseUrl + localFilePath.substr(options.subfolder.length);
+        ns.print(`Trying to update "${localFilePath}" from ${remoteFilePath} ...`);
+        if (await ns.wget(`${remoteFilePath}?ts=${new Date().getTime()}`, localFilePath))
+            ns.tprint(`SUCCESS: Updated "${localFilePath}" to the latest from ${remoteFilePath}`);
         else
-            ns.tprint(`WARNING: ${filename} was not updated. (Currently running or not located at ${baseUrl}${filename} )`)
+            ns.tprint(`WARNING: "${localFilePath}" was not updated. (Currently running or not located at ${remoteFilePath} )`)
     }
 }
