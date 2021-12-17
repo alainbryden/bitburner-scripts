c7f18d63458229bf3115e97397ba0febbf3a0c08
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2021-12-17 14:08:53 -0400

    Git-pull ignore temp folder

--- git-pull.js
+++ git-pull.js
@@ -22,7 +22,7 @@ export async function main(ns) {
     const options = ns.flags(argsSchema);
     const baseUrl = `https://raw.githubusercontent.com/${options.github}/${options.repository}/${options.branch}/`;
     const filesToDownload = options['new-file'].concat(options.download.length > 0 ? options.download : ns.ls('home')
-        .filter(name => !name.endsWith(".exe") && !name.endsWith(".msg") && !name.endsWith(".lit")));
+        .filter(name => !name.endsWith(".exe") && !name.endsWith(".msg") && !name.endsWith(".lit") && !name.startsWith("/Temp/")));
     for (const localFilePath of filesToDownload) {
         const remoteFilePath = baseUrl + localFilePath.substr(options.subfolder.length);
         ns.print(`Trying to update "${localFilePath}" from ${remoteFilePath} ...`);
