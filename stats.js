ce5e312e1199fa05a960a90ce347114167762f8b
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2021-12-20 12:29:38 -0400

    Hide stock when stockmaster hud is visible

--- stats.js
+++ stats.js
@@ -1,12 +1,24 @@
 import { formatNumberShort, formatMoney, getNsDataThroughFile } from './helpers.js'
 
+const argsSchema = [
+    ['hide-stocks', false],
+];
+
+export function autocomplete(data, args) {
+    data.flags(argsSchema);
+    return [];
+}
+
 /** @param {NS} ns **/
 export async function main(ns) {
+    const options = ns.flags(argsSchema);
     const doc = eval('document');
     const hook0 = doc.getElementById('overview-extra-hook-0');
     const hook1 = doc.getElementById('overview-extra-hook-1');
     let stkSymbols = null;
-    try { stkSymbols = await getNsDataThroughFile(ns, `ns.stock.getSymbols()`, '/Temp/stock-symbols.txt'); } catch { }
+    if (!options['hide-stocks']) {
+        try { stkSymbols = await getNsDataThroughFile(ns, `ns.stock.getSymbols()`, '/Temp/stock-symbols.txt'); } catch { }
+    }
     while (true) {
         try {
             const headers = []
@@ -23,13 +35,16 @@ export async function main(ns) {
                 values.push("Liquidating");
             }
 
-            if (stkSymbols) {
-                headers.push("Stock");
+            let stockmasterHud = doc.getElementById("stock-display-1"); // Don't add stocks if the stockmaster built-in HUD is active
+            if (stkSymbols && !stockmasterHud) {
                 const stkPortfolio = await getNsDataThroughFile(ns, JSON.stringify(stkSymbols) +
                     `.map(sym => ({ sym, pos: ns.stock.getPosition(sym), ask: ns.stock.getAskPrice(sym), bid: ns.stock.getBidPrice(sym) }))` +
-                    `.reduce((total, stk) => total + stk.pos[0] * stk.bid + stk.pos[2] * (stk.ask * 2 - stk.bid), 0)`,
-                    '/Temp/stock-portfolio-value.txt')
-                values.push(formatMoney(stkPortfolio));
+                    `.reduce((total, stk) => total + stk.pos[0] * stk.bid + stk.pos[2] * (stk.pos[3] * 2 - stk.ask) -100000 * (stk.pos[0] + stk.pos[2] > 0 ? 1 : 0), 0)`,
+                    '/Temp/stock-portfolio-value.txt');
+                if (stkPortfolio > 0) {
+                    headers.push("Stock");
+                    values.push(formatMoney(stkPortfolio));
+                }
             }
             headers.push("ScrInc");
             values.push(formatMoney(ns.getScriptIncome()[0], 3, 2) + '/sec');
