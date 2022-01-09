20d109682262a0115d0c4267e7879b715df66319
ShawnPatton <generalscpatton@gmail.com>
2022-01-08 22:27:42 -0600

    Reworked for vastly improved gains

--- farm-intelligence.js
+++ farm-intelligence.js
@@ -1,11 +1,60 @@
+import {
+    formatDuration, disableLogs
+} from './helpers.js'
+
+const argsSchema = [
+    ['trips-per-cycle', 1000],
+    ['money-threshold', 1000000000000]
+];
+
+export function autocomplete(data, args) {
+    data.flags(argsSchema);
+    return [];
+}
+
 /** @param {NS} ns **/
 export async function main(ns) {
-    let program = ns.args.length > 0 ? ns.args[0] : "DeepscanV1.exe";
+    disableLogs(ns, ["travelToCity", "sleep"]);
     ns.tail();
-    while (true) {
-        while (ns.isBusy())
-            await ns.sleep(1000);
-        ns.rm(program);
-        ns.createProgram(program);
+    let options = ns.flags(argsSchema);
+    let tripsPerCycle = options['trips-per-cycle'];
+    let moneyThreshold = options['money-threshold'];
+    ns.print(`trips-per-cycle: ` + tripsPerCycle);
+    ns.print(`money-threshold: ` + moneyThreshold);
+    let justStarted = true;
+    let previousInt = ns.getPlayer().intelligence;
+    let currentInt = previousInt;
+    let previousLevelTime = Date.now();
+    let levelupTime;
+    let cycles = 0;
+    let duration = 0;
+    let tripsPerLevel = 0;
+    let tripsPerMs = 0;
+    ns.print(`Starting Script at Int ` + currentInt);
+    while(true) {
+        while(ns.getPlayer().money > moneyThreshold) {
+            for(let i = 0; i < tripsPerCycle; i++) {
+                ns.travelToCity("Aevum");
+                ns.travelToCity("Sector-12");
+            }
+            await ns.sleep(1);
+            cycles++;
+            if(previousInt != ns.getPlayer().intelligence) {
+                currentInt = ns.getPlayer().intelligence;
+                levelupTime = Date.now();
+                duration = levelupTime - previousLevelTime;
+                tripsPerLevel = cycles * tripsPerCycle * 2;
+                tripsPerMs = Math.floor(tripsPerLevel / duration);
+                ns.print(`Level Up: Int ` + currentInt + (justStarted ? ` Partial` : ` Full`) + ` Level in `
+                  + formatDuration(duration) + ` & ` + (tripsPerLevel) + ` Travels`);
+                ns.print(`Approximately ` + tripsPerMs + ` Trips/Millisecond`);
+                previousLevelTime = levelupTime;
+                previousInt = currentInt;
+                justStarted = false;
+                cycles = 0;
+            }
+        }
+        await ns.sleep(10000);
+        ns.print(`Below money threshold, waiting 10 seconds`);
     }
 }
