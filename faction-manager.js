29a05277bf1a91fb76af23538f197a0e1c8cbf50
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-01-08 23:50:36 -0400

    Clear warnings when SF4 is not available or is under-levelled

--- faction-manager.js
+++ faction-manager.js
@@ -84,6 +84,12 @@ export async function main(ns) {
     const sort = unshorten(options.sort); // Support the user leaving off the _mult suffix
     playerData = await getNsDataThroughFile(ns, 'ns.getPlayer()', '/Temp/player-info.txt');
     const ownedSourceFiles = await getActiveSourceFiles(ns);
+    const sf4Level = ownedSourceFiles[4] || 0;
+    if (sf4Level == 0)
+        log(ns, `WARNING: This script makes heavy use of singularity functions. Without SF4, you're unlikely to get it working.`);
+    else if (sf4Level < 3)
+        log(ns, `WARNING: This script makes heavy use of singularity functions, which are quite expensive before you have SF4.3. ` +
+            `Unless you have a lot of free RAM for temporary scripts, you may get runtime errors.`);
     const sf11Level = ownedSourceFiles[11] || 0;
     augCountMult = [1.9, 1.824, 1.786, 1.767][sf11Level];
     log(ns, `Player has sf11Level ${sf11Level}, so the multiplier after each aug purchased is ${augCountMult}.`);
@@ -405,9 +411,9 @@ async function manageFilteredSubset(ns, outputRows, subsetName, subset, printLis
             const factionsWithAugAndInvite = factionsWithAug.filter(f => f.invited || f.joined).sort((a, b) => b.favor - a.favor);
             const factionWithMostFavor = factionsWithAugAndInvite[0] ?? factionsWithAug[0];
             if (getFrom != factionsWithAug[0].name && factionsWithAug[0] != factionsWithAugAndInvite[0])
-                outputRows.push(`Suggested that you earn an invitation to faction ${factionsWithAug[0].name} to make it easier to get rep for ${strNF} since it has the most favor (${factionsWithAug[0].favor}).`);
+                outputRows.push(`SUGGESTION: Earn an invitation to faction ${factionsWithAug[0].name} to make it easier to get rep for ${strNF} since it has the most favor (${factionsWithAug[0].favor}).`);
             else if (factionsWithAug[0].joined && !factionsWithAug[0].donationsUnlocked)
-                outputRows.push(`Suggested that you do some work for faction ${factionsWithAug[0].name} to qickly earn rep for ${strNF} since it has the most favor (${factionsWithAug[0].favor}).`);
+                outputRows.push(`SUGGESTION: Do some work for faction ${factionsWithAug[0].name} to qickly earn rep for ${strNF} since it has the most favor (${factionsWithAug[0].favor}).`);
             else if ((!getFrom || factionData[getFrom].favor < factionWithMostFavor.favor) && factionWithMostFavor.invited) {
                 outputRows.push(`Attempting to join faction ${factionWithMostFavor.name} to make it easier to get rep for ${strNF} since it has the most favor (${factionWithMostFavor.favor}).`);
                 options['force-join'].push(factionWithMostFavor.name);
