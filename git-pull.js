336b8f1d0b26d9557686395c5e94baafbd966332
malaclypse2 <malaclypse2@gmail.com>
2022-01-10 02:24:26 -0500

    update git-pull to point to my fork

--- git-pull.js
+++ git-pull.js
@@ -1,7 +1,7 @@
 let options;
 const argsSchema = [
-    ['github', 'alainbryden'],
-    ['repository', 'bitburner-scripts'],
+    ['github', 'malaclypse2'],
+    ['repository', 'bitburner-scripts-1'],
     ['branch', 'main'],
     ['download', []], // By default, all supported files in the repository will be downloaded. Override with just a subset of files here
     ['new-file', []], // If a repository listing fails, only files returned by ns.ls() will be downloaded. You can add additional files to seek out here.
