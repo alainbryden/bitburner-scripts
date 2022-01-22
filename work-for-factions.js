35b65ea49a47d6a83d4d90c0e162b3dfe81c0fdf
Alexander Lichter <manniL@gmx.net>
2022-01-22 18:30:15 +0100

    fix(work-for-faction): do not stop script when SF 4 < 4.3 (#41)

--- work-for-factions.js
+++ work-for-factions.js
@@ -122,7 +122,7 @@ export async function main(ns) {
     if (!(4 in dictSourceFiles))
         return ns.tprint("ERROR: You cannot automate working for factions until you have unlocked singularity access (SF4).");
     else if (dictSourceFiles[4] < 3)
-        return ns.tprint(`WARNING: Singularity functions are much more expensive with lower levels of SF4 (you have SF4.${dictSourceFiles[4]}). ` +
+        ns.tprint(`WARNING: Singularity functions are much more expensive with lower levels of SF4 (you have SF4.${dictSourceFiles[4]}). ` +
             `You may encounter RAM issues with and have to wait until you have more RAM available to run this script successfully.`);
 
     let bitnodeMults = await tryGetBitNodeMultipliers(ns); // Find out the current bitnode multipliers (if available)
