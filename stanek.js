00d4fc5058274a9af479af024d4ce1051a9751da
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-02-13 19:22:47 -0400

    let some stuff other than stanek go on.

--- stanek.js
+++ stanek.js
@@ -1,6 +1,7 @@
 import { getNsDataThroughFile, disableLogs, formatNumberShort, getFilePath, waitForProcessToComplete } from './helpers.js'
 
 const maxCharges = 10000; // Don't bother adding charges beyond this amount (diminishing returns - num charges is ^0.07 )
+const idealReservedRam = 32; // Reserve this much RAM, if it wouldnt make a big difference anyway
 
 /** @param {NS} ns 
  * Maximizes charge on staney fragments based on current home RAM.
@@ -13,9 +14,12 @@ export async function main(ns) {
             ns.print(`Fragment ${String(fragment.id).padStart(2)} at [${fragment.x},${fragment.y}] ` +
                 `charge num: ${formatNumberShort(fragment.numCharge)} avg: ${formatNumberShort(fragment.avgCharge)}`);
         for (const fragment of fragments) {
-            const threads = Math.floor((ns.getServerMaxRam('home') - ns.getServerUsedRam('home')) / 2.0);
+            let availableRam = ns.getServerMaxRam('home') - ns.getServerUsedRam('home');
+            if (idealReservedRam / availableRam < 0.05)
+                availableRam -= idealReservedRam;
+            const threads = Math.floor(availableRam / 2.0);
             // Only charge if we will not be bringing down the average
-            if (threads < fragment.avgCharge || fragment.numCharge > maxCharges) continue;
+            if (threads < fragment.avgCharge * 0.99 || fragment.numCharge > maxCharges) continue;
             const pid = ns.run(getFilePath('/stanek.js.charge.js'), threads, fragment.x, fragment.y);
             await waitForProcessToComplete(ns, pid);
         }
