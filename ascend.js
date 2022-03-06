dd031631332da0f5ae649471f523df5c3d8a4950
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-03-06 12:49:21 -0400

    Don't rely on timing, check for actions to have completed

--- ascend.js
+++ ascend.js
@@ -1,10 +1,10 @@
-import { runCommand, getNsDataThroughFile, getActiveSourceFiles, log } from './helpers.js'
+import { runCommand, waitForProcessToComplete, getNsDataThroughFile, getActiveSourceFiles, log } from './helpers.js'
 
 const argsSchema = [
     ['force', false], // There will be sanity checks - use this option to bypass them
-    ['scripts-to-kill', ['daemon.js', 'gangs.js', 'sleeves.js', 'work-for-factions.js']], // Kill these scripts at launch
+    ['scripts-to-kill', ['daemon.js', 'gangs.js', 'sleeves.js', 'work-for-factions.js', 'farm-intelligence.js']], // Kill these money-spending scripts at launch
     ['reset', false], // By default (for now) does not actually install augmentations unless you use this flag
-    ['on-reset-script', 'daemon.js'], // Spawn this script when max-charges is reached
+    ['on-reset-script', 'daemon.js'], // Spawn this script after installing augmentations (Note: Args not supported)
 ];
 
 export function autocomplete(data, args) {
@@ -27,29 +27,42 @@ export async function main(ns) {
     // - We should be able to install ~10 augs or so after maxing home ram purchases?
     // - We should have installed 
     // - Have a force option to override (and pass-through to faction manager)
+    const playerData = await getNsDataThroughFile(ns, 'ns.getPlayer()', '/Temp/player-info.txt');
 
     // Kill any other scripts that may interfere with our spending
-    await runCommand(ns, `ns.ps().filter(s => ${JSON.stringify(options['scripts-to-kill'])}.includes(s.filename)).forEach(s => ns.kill(s.pid));`, '/Temp/kill-processes.js');
-    await ns.sleep(400); // Wait a couple ticks for things to die
+    let pid = await runCommand(ns, `ns.ps().filter(s => ${JSON.stringify(options['scripts-to-kill'])}.includes(s.filename)).forEach(s => ns.kill(s.pid));`, '/Temp/kill-processes.js');
+    await waitForProcessToComplete(ns, pid, true); // Wait for the script to shut down, indicating it has shut down other scripts
 
     // STEP 1: Liquidate Stocks and (SF9) Hacknet Hashes
     log(ns, 'Sell stocks and hashes...', true, 'info');
-    ns.run('stockmaster.js', 1, '--liquidate');
     ns.run('spend-hacknet-hashes.js', 1, '--liquidate');
-    await ns.sleep(1000); // Takes a bit of time for things to get sold
+    let stockValue = null;
+    if (playerData.hasTixApiAccess) {
+        ns.run('stockmaster.js', 1, '--liquidate');
+        const stkSymbols = await getNsDataThroughFile(ns, `ns.stock.getSymbols()`, '/Temp/stock-symbols.txt');
+        while (stockValue !== 0) { // It takes a bit of time for things to get sold. Wait until we see no stock holdings
+            stockValue = await getNsDataThroughFile(ns, JSON.stringify(stkSymbols) +
+                `.map(sym => ({ sym, pos: ns.stock.getPosition(sym), ask: ns.stock.getAskPrice(sym), bid: ns.stock.getBidPrice(sym) }))` +
+                `.reduce((total, stk) => total + stk.pos[0] * stk.bid + stk.pos[2] * (stk.pos[3] * 2 - stk.ask) -100000 * (stk.pos[0] + stk.pos[2] > 0 ? 1 : 0), 0)`,
+                '/Temp/stock-portfolio-value.txt');
+            log(ns, 'INFO: Waiting for stocks to be sold...', false, 'info');
+            await ns.sleep(200);
+        }
+    }
 
     // STEP 2: Buy Home RAM Upgrades (more important than squeezing in a few extra augs)
     log(ns, 'Try Upgrade Home RAM...', true, 'info');
-    ns.run('Tasks/ram-manager.js', 1, '--reserve', '0', '--budget', '0.8');
-    await ns.sleep(200); // Give it time to make its purchases
-    // TODO: (SF13) If Stanek is unlocked, and we have not yet accepted Stanek's gift, now's our last chance to do it
+    pid = ns.run('Tasks/ram-manager.js', 1, '--reserve', '0', '--budget', '0.8');
+    await waitForProcessToComplete(ns, pid, true); // Wait for the script to shut down, indicating it has bought all it can.
+
+    // TODO: (SF13) If Stanek is unlocked, and we have not yet accepted Stanek's gift, now's our last chance to do it (before purchasing augs)
 
     // STEP 3: Buy as many augmentations as possible
     log(ns, 'Purchasing augmentations...', true, 'info');
     const facmanArgs = ['--purchase', '-v'];
     if (options.force) facmanArgs.push('--force')
-    ns.run('faction-manager.js', 1, ...facmanArgs);
-    await ns.sleep(400); // Give it time to make its purchases
+    pid = ns.run('faction-manager.js', 1, ...facmanArgs);
+    await waitForProcessToComplete(ns, pid, true); // Wait for the script to shut down, indicating it is done.
 
     // Sanity check, if we are not slated to install any augmentations, ABORT
     // Get owned + purchased augmentations, then installed augmentations. Ensure there's a difference
@@ -60,31 +73,48 @@ export async function main(ns) {
         if (!options.force) return;
     }
 
-    // STEP 4: (SF10) Buy whatever sleeve upgrades we can afford
+    // STEP 4: Try to Buy 4S data / API if we haven't already and can afford it (although generally stockmaster.js would have bought these if it could)
+    log(ns, 'Checking on Stock Market upgrades...', true, 'info');
+    if (playerData.hasTixApiAcces && !playerData.has4SDataTixApi)
+        await getNsDataThroughFile(ns, 'ns.stock.purchase4SMarketDataTixApi()', '/Temp/purchase-4s-api.txt');
+    if (playerData.hasTixApiAcces && !playerData.has4SData)
+        await getNsDataThroughFile(ns, 'ns.stock.purchase4SMarketData()', '/Temp/purchase-4s.txt');
+
+    // STEP 5: (SF10) Buy whatever sleeve upgrades we can afford
     if (10 in dictSourceFiles) {
         log(ns, 'Try Upgrade Sleeves...', true, 'info');
         ns.run('sleeve.js', 1, '--reserve', '0', '--aug-budget', '1', '--min-aug-batch', '1', '--buy-cooldown', '0');
-        await ns.sleep(200); // Give it time to make its purchases
+        await ns.sleep(500); // Give it time to make its initial purchases. Note that we do not block on the process shutting down - it will keep running.
     }
 
-    // STEP 5: (SF2) Buy whatever gang equipment we can afford
+    // STEP 6: (SF2) Buy whatever gang equipment we can afford
     if (2 in dictSourceFiles) {
         log(ns, 'Try Upgrade Gangs...', true, 'info');
         ns.run('gangs.js', 1, '--reserve', '0', '--augmentations-budget', '1', '--equipment-budget', '1');
-        await ns.sleep(200); // Give it time to make its purchases
+        await ns.sleep(500); // Give it time to make its initial purchases. Note that we do not block on the process shutting down - it will keep running.
     }
 
-    // STEP 6: Buy whatever home CPU upgrades we can afford
+    // STEP 7: Buy whatever home CPU upgrades we can afford
     log(ns, 'Try Upgrade Home Cores...', true, 'info');
-    await runCommand(ns, `while(ns.upgradeHomeCores()); { await ns.sleep(10); }`, '/Temp/upgrade-home-ram.js');
-    await ns.sleep(200); // Give it time to make its purchases
+    pid = await runCommand(ns, `while(ns.upgradeHomeCores()); { await ns.sleep(10); }`, '/Temp/upgrade-home-ram.js');
+    await waitForProcessToComplete(ns, pid, true); // Wait for the script to shut down, indicating it has bought all it can.
+
     // TODO: If in corporation, and buyback shares is available, buy as many as we can afford
     // TODO: Anything to do for Bladeburner?
 
+    // WAIT: For money to stop decreasing, so we know that external scripts have bought what they could.
+    log(ns, 'Waiting for purchasing to stop...', true, 'info');
+    let money = 0, lastMoney = 0, ticksWithoutPurchases = 0;
+    while (ticksWithoutPurchases < 25) { // 25 * 200ms (game tick time) = 5 seconds
+        while (lastMoney == (money = await getNsDataThroughFile(ns, `ns.getServerMoneyAvailable('home')`, '/Temp/player-money.txt')))
+            await ns.sleep(50); // Wait for game to tick (money to change)
+        ticksWithoutPurchases = money < lastMoney ? 0 : ticksWithoutPurchases + 1;
+    }
+
     // FINALLY: If configured, soft reset
-    log(ns, 'Catch you on the flippity-flip', true, 'success');
+    log(ns, '\nCatch you on the flippity-flip\n', true, 'success');
     if (options.reset) {
-        await ns.sleep(1000); // Pause for effect (really, give everything time to make any additional rounds of purchases)
+        await ns.sleep(1000); // Pause for effect?
         await runCommand(ns, `ns.installAugmentations('${options['on-reset-script']}')`, '/Temp/soft-reset.js');
     }
 }
