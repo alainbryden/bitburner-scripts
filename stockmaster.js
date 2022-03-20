8b9966fab054f48804a84f90910f870d604f492f
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-03-20 13:32:30 -0300

    Fix using too much history for reversal detection

--- stockmaster.js
+++ stockmaster.js
@@ -319,7 +319,7 @@ async function updateForecast(ns, allStocks, has4s) {
         // We want stocks that have the best expected return, averaged over a long window for greater precision, but the game will occasionally invert probabilities
         // (45% chance every 75 updates), so we also compute a near-term forecast window to allow for early-detection of inversions so we can ditch our position.
         stk.nearTermForecast = forecast(stk.priceHistory.slice(0, nearTermForecastWindowLength));
-        let preNearTermWindowProb = forecast(stk.priceHistory.slice(nearTermForecastWindowLength)); // Used to detect the probability before the potential inversion event.
+        let preNearTermWindowProb = forecast(stk.priceHistory.slice(nearTermForecastWindowLength, nearTermForecastWindowLength + marketCycleLength)); // Used to detect the probability before the potential inversion event.
         // Detect whether it appears as though the probability of this stock has recently undergone an inversion (i.e. prob => 1 - prob)
         stk.possibleInversionDetected = has4s ? detectInversion(stk.prob, stk.lastTickProbability || stk.prob) : detectInversion(preNearTermWindowProb, stk.nearTermForecast);
         stk.lastTickProbability = stk.prob;
@@ -345,7 +345,7 @@ async function updateForecast(ns, allStocks, has4s) {
     for (const stk of allStocks) {
         // Don't "trust" (act on) a detected inversion unless it's near the time when we're capable of detecting market cycle start. Avoids most false-positives.
         if (stk.possibleInversionDetected && (has4s && detectedCycleTick == 0 ||
-            (!has4s && (detectedCycleTick > nearTermForecastWindowLength / 2 - 1) && (detectedCycleTick <= nearTermForecastWindowLength + inversionLagTolerance))))
+            (!has4s && (detectedCycleTick >= nearTermForecastWindowLength / 2) && (detectedCycleTick <= nearTermForecastWindowLength + inversionLagTolerance))))
             stk.lastInversion = detectedCycleTick; // If we "trust" a probability inversion has occurred, probability will be calculated based on only history since the last inversion.
         else
             stk.lastInversion++;
