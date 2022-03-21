369725cbc6f8cbb5c5cca0f23c817f662d045f3c
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-03-20 21:37:58 -0300

    Significantly reduce the chance of a random training session, but increase training time after ascending

--- gangs.js
+++ gangs.js
@@ -40,13 +40,13 @@ let importantStats = [];
 
 let options;
 const argsSchema = [
-    ['training-percentage', 0.20], // Spend this percent of time training gang members versus doing crime
+    ['training-percentage', 0.05], // Spend this percent of time randomly training gang members versus doing crime
     ['no-training', false], // Don't train unless all other tasks generate no gains
     ['no-auto-ascending', false], // Don't ascend members
     ['ascend-multi-threshold', 1.05], // Ascend member #12 if a primary stat multi would increase by more than this amount
     ['ascend-multi-threshold-spacing', 0.05], // Members will space their acention multis by this amount to ensure they are ascending at different rates 
     // Note: given the above two defaults, members would ascend at multis [1.6, 1.55, 1.50, ..., 1.1, 1.05] once you have 12 members.
-    ['min-training-ticks', 20], // Require this many ticks of training after ascending or recruiting
+    ['min-training-ticks', 40], // Require this many ticks of training after ascending or recruiting to rebuild stats
     ['reserve', null], // Reserve this much cash before determining spending budgets (defaults to contents of reserve.txt if not specified)
     ['augmentations-budget', null], // Percentage of non-reserved cash to spend per tick on permanent member upgrades (If not specified, uses defaultMaxSpendPerTickPermanentEquipment)
     ['equipment-budget', null], // Percentage of non-reserved cash to spend per tick on permanent member upgrades (If not specified, uses defaultMaxSpendPerTickTransientEquipment)
