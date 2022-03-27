10a757277777c8fadd0b7648217d2a08595f6730
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-03-27 15:02:56 -0300

    Added the ability to spend on multiple things at once from the same instance

--- spend-hacknet-hashes.js
+++ spend-hacknet-hashes.js
@@ -6,17 +6,21 @@ const argsSchema = [
     ['l', false], // Turn all hashes into money
     ['liquidate', false],
     ['interval', 1000], // Rate at which the program runs and spends hashes
-    ['spend-on', 'Sell for Money'],
+    ['spend-on', ['Sell for Money']],
     ['spend-on-server', undefined],
 ];
 
-const purchaseOptions = ['Sell for Money', 'Sell for Corporation Funds', 'Exchange for Corporation Research', 'Generate Coding Contract', 'Improve Studying', 'Improve Gym Training'];
+const basicSpendOptions = ['Sell for Money', 'Generate Coding Contract', 'Improve Studying', 'Improve Gym Training',
+    'Sell for Corporation Funds', 'Exchange for Corporation Research', 'Exchange for Bladeburner Rank', 'Exchange for Bladeburner SP'];
+const parameterizedSpendOptions = ['Reduce Minimum Security', 'Increase Maximum Money'];
+const purchaseOptions = basicSpendOptions.concat(parameterizedSpendOptions);
 
 export function autocomplete(data, args) {
     data.flags(argsSchema);
     const lastFlag = args.length > 1 ? args[args.length - 2] : null;
     if (lastFlag == "--spend-on") // Provide a couple auto-complete options to facilitate these arguments with spaces in them
-        return purchaseOptions.map(f => f.replaceAll(" ", "_")).sort().concat(purchaseOptions.map(f => `'${f}'`).sort());
+        return purchaseOptions.map(f => f.replaceAll(" ", "_"))
+            .concat(purchaseOptions.map(f => `'${f}'`));
     return [];
 }
 
@@ -26,7 +30,7 @@ export async function main(ns) {
     const verbose = options.v || options.verbose;
     const liquidate = options.l || options.liquidate;
     const interval = options.interval;
-    const toBuy = options['spend-on'].replaceAll("_", " ");
+    const toBuy = options['spend-on'].map(s => s.replaceAll("_", " "));
     const spendOnServer = options['spend-on-server']?.replaceAll("_", " ") ?? undefined;
     disableLogs(ns, ['sleep']);
     ns.print(`Starting spend-hacknet-hashes.js to ensure no hashes go unspent. Will check in every ${formatDuration(interval)}`);
@@ -46,12 +50,14 @@ export async function main(ns) {
         let reserve = 10 + globalProduction * interval / 1000; // If we are this far from our capacity, start spending
         let success = true;
         while (success && ns.hacknet.numHashes() > (liquidate ? 4 : capacity - reserve))
-            success = ns.hacknet.spendHashes(toBuy, spendOnServer);
-        if (!success)
-            ns.print(`Weird, failed to spend hashes. (Have: ${ns.hacknet.numHashes()} Capacity: ${ns.hacknet.hashCapacity()}`);
-        if (verbose && ns.hacknet.numHashes() < startingHashes)
-            ns.print(`Spent ${(startingHashes - ns.hacknet.numHashes()).toFixed(0)} hashes` +
-                (liquidate ? '' : ` to avoid reaching capacity (${capacity})`) + ` at ${globalProduction.toPrecision(3)} hashes per second`);
+            for (const spendAction of toBuy) {
+                success = ns.hacknet.spendHashes(spendAction, parameterizedSpendOptions.includes(spendAction) ? spendOnServer : undefined);
+                if (!success && !liquidate)
+                    ns.print(`Weird, failed to spend hashes on '${spendAction}'. (Have: ${ns.hacknet.numHashes()} Capacity: ${ns.hacknet.hashCapacity()}`);
+                if (verbose && ns.hacknet.numHashes() < startingHashes)
+                    ns.print(`Spent ${(startingHashes - ns.hacknet.numHashes()).toFixed(0)} hashes on '${spendAction}'` +
+                        (liquidate ? '' : ` to avoid reaching capacity (${capacity})`) + ` at ${globalProduction.toPrecision(3)} hashes per second`);
+            }
         await ns.sleep(interval);
     }
 }
