d60d8be82582c25481fca8a4bcdafe18214a4db5
alendaris <102333831+alendaris@users.noreply.github.com>
2022-03-27 15:59:19 -0500

    Update git-pull.js

--- git-pull.js
+++ git-pull.js
@@ -1,8 +1,8 @@
 let options;
 const argsSchema = [
-    ['github', 'alainbryden'],
+    ['github', 'alendaris'],
     ['repository', 'bitburner-scripts'],
-    ['branch', 'main'],
+    ['branch', 'production'],
     ['download', []], // By default, all supported files in the repository will be downloaded. Override with just a subset of files here
     ['new-file', []], // If a repository listing fails, only files returned by ns.ls() will be downloaded. You can add additional files to seek out here.
     ['subfolder', ''], // Can be set to download to a sub-folder that is not part of the remote repository structure
