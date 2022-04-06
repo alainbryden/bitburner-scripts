06c1647fe481f29080217541dbb681092c854f84
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-04-06 16:00:45 -0300

    API Changes

--- stanek.js.reset.js
+++ stanek.js.reset.js
@@ -3,7 +3,7 @@
 export async function main(ns) {
     for (const fragment of ns.stanek.activeFragments()) {
         const [id, x, y, r] = [fragment.id, fragment.x, fragment.y, fragment.rotation];
-        ns.stanek.remove(x, y);
-        ns.stanek.place(x, y, r, id);
+        ns.stanek.removeFragment(x, y);
+        ns.stanek.placeFragment(x, y, r, id);
     }
 }
