7da49329792aa9404f16bc5b5f9907aeef082d3f
Stephen Reindl <stephen.reindl@outlook.de>
2022-04-12 20:19:34 +0200

    feat(git): handel git update correct
    Closes #2

--- dist/Remote/share.js
+++
@@ -1,4 +0,0 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-	await ns.share()
-}
--- dist/cleanup.js
+++
@@ -1,5 +0,0 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-    for (let file of ns.ls('home', '/Temp/'))
-        ns.rm(file);
-}
