7ecf1cc8644be46f3f30caec3ea1de24d350a84d
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-04-14 12:34:42 -0300

    Minor improvements

--- remove-worst-server.js
+++ remove-worst-server.js
@@ -1,9 +1,7 @@
-import { getNsDataThroughFile, runCommand } from './helpers.js'
+import { getNsDataThroughFile, runCommand, formatRam } from './helpers.js'
 
-/** 
- * Remove the worst owned server respective of RAM
- * @param {NS} ns
- **/
+/** @param {NS} ns
+ * Remove the worst owned server respective of RAM */
 export async function main(ns) {
     const purchasedServers = await getNsDataThroughFile(ns, 'ns.getPurchasedServers()', '/Temp/purchased-servers.txt');
     if (purchasedServers.length === 0)
@@ -12,16 +10,16 @@ export async function main(ns) {
     const minServer = purchasedServers.reduce((minServer, currServer) => {
         const currRam = ns.getServerMaxRam(currServer);
         return minServer.ram > currRam ? { name: currServer, ram: currRam } : minServer;
-    }, { name: "", ram: 2 ** 20 });
+    }, { name: null, ram: Number.MAX_VALUE });
 
     if (!minServer.name)
-        return ns.tprint(`Nothing to delete - all ${purchasedServers.length} servers have the maximum RAM (${2 ** 20} GB)`);
+        return ns.tprint(`Nothing to delete - all ${purchasedServers.length} servers have the maximum RAM (2^20 or ${formatRam(2 ** 20)})`);
 
     // Flag the server for deletion with a file - daemon should check for this and stop scheduling against it.
-    await runCommand(ns, `await ns.scp("/Flags/deleting.txt", "${minServer.name}")`, '/Temp/flag-server-for-deletion.js');
-    const success = await getNsDataThroughFile(ns, `ns.deleteServer("${minServer.name}")`, '/Temp/try-delete-server-result.txt');
+    await runCommand(ns, `await ns.scp("/Flags/deleting.txt", ns.args[0])`, '/Temp/flag-server-for-deletion.js', [minServer.name]);
+    const success = await getNsDataThroughFile(ns, `ns.deleteServer(ns.args[0])`, '/Temp/deleteServer.txt', [minServer.name]);
     if (success)
-        ns.tprint(`Deleted ${minServer.name} which had ${minServer.ram} GB of RAM (${purchasedServers.length - 1} servers remaining).`);
+        ns.tprint(`Deleted ${minServer.name} which had ${formatRam(minServer.ram)} of RAM (${purchasedServers.length - 1} servers remaining).`);
     else
-        ns.tprint(`Failed to delete ${minServer.name} (${minServer.ram} GB of RAM) - scripts are likely still running`);
+        ns.tprint(`Failed to delete ${minServer.name} (${formatRam(minServer.ram)} of RAM) - scripts are likely still running`);
 }
