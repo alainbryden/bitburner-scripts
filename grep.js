eb1136998e019969102c71a22e77c7697d12b4c1
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-04-14 21:01:32 -0300

    Fun little helper script to search the contents of files

---
+++ grep.js
@@ -0,0 +1,19 @@
+export function autocomplete(data, args) {
+    const lastFlag = args.length > 1 ? args[args.length - 2] : null;
+    return lastFlag ? [] : data.scripts;
+}
+
+/** @param {NS} ns */
+export async function main(ns) {
+    const txt = ns.args.length == 0 ? "" : ns.read(ns.args[0]);
+    if (!txt) return ns.tprint("ERROR: The first argument must be a file to search (second argument is the text to search for)");
+    const search = ns.args.length < 2 ? "" : ns.args[1];
+    if (!search) return ns.tprint("ERROR: Missing second argument (search string).");
+    const output = [];
+    txt.split("\n").forEach((row, i) => {
+        if (row.includes(search))
+            output.push(`${i}`.padStart(3) + `: ${row}`);
+    })
+    ns.tprint(output.length == 0 ? `Search string "${search}" not found in file ${ns.args[0]}` :
+        `Found ${output.length} occurrences of the string "${search}" in file ${ns.args[0]}:\n${output.join("\n")}`);
+}
