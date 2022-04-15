8f85d7ede52cc58eaef86f6c083a41b1b480b70b
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-04-15 13:35:40 -0300

    Add 4 new coding contract solvers ( Fix #84 )
    Accepting PR #84 to add some solvers with some minor modifications

--- Tasks/contractor.js.solver.js
+++ Tasks/contractor.js.solver.js
@@ -97,6 +97,22 @@ const codingContractTypesMetadata = [{
     },
 },
 {
+    name: 'Total Ways to Sum II',
+    solver: function (data) {
+        const n = data[0];
+        const s = data[1];
+        const ways = [1];
+        ways.length = n + 1;
+        ways.fill(0, 1);
+        for (let i = 0; i < s.length; i++) {
+            for (let j = s[i]; j <= n; j++) {
+                ways[j] += ways[j - s[i]];
+            }
+        }
+        return ways[n];
+    },
+},
+{
     name: 'Spiralize Matrix',
     solver: function (data, ans) {
         var spiral = []
@@ -158,6 +174,31 @@ const codingContractTypesMetadata = [{
     },
 },
 {
+    name: 'Array Jumping Game II',
+    solver: function (data) {
+        const n = data.length;
+        let reach = 0;
+        let jumps = 0;
+        let lastJump = -1;
+        while (reach < n - 1) {
+            let jumpedFrom = -1;
+            for (let i = reach; i > lastJump; i--) {
+                if (i + data[i] > reach) {
+                    reach = i + data[i];
+                    jumpedFrom = i;
+                }
+            }
+            if (jumpedFrom === -1) {
+                jumps = 0;
+                break;
+            }
+            lastJump = jumpedFrom;
+            jumps++;
+        }
+        return jumps;
+    },
+},
+{
     name: 'Merge Overlapping Intervals',
     solver: function (data) {
         var intervals = data.slice()
@@ -167,8 +208,8 @@ const codingContractTypesMetadata = [{
         var result = []
         var start = intervals[0][0]
         var end = intervals[0][1]
-        for (var _i = 0, intervals_1 = intervals; _i < intervals_1.length; _i++) {
-            var interval = intervals_1[_i]
+        for (var i = 0, intervals_1 = intervals; i < intervals_1.length; i++) {
+            var interval = intervals_1[i]
             if (interval[0] <= end) {
                 end = Math.max(end, interval[1])
             } else {
@@ -238,8 +279,8 @@ const codingContractTypesMetadata = [{
         var hold2 = Number.MIN_SAFE_INTEGER
         var release1 = 0
         var release2 = 0
-        for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
-            var price = data_1[_i]
+        for (var i = 0, data_1 = data; i < data_1.length; i++) {
+            var price = data_1[i]
             release2 = Math.max(release2, hold2 + price)
             hold2 = Math.max(hold2, release1 - price)
             release1 = Math.max(release1, hold1 + price)
@@ -413,6 +454,87 @@ const codingContractTypesMetadata = [{
         var result = []
         helper(result, '', num, target, 0, 0, 0)
         return result
+    },
+},
+{
+    name: 'HammingCodes: Integer to encoded Binary',
+    solver: function (value) {
+        // Calculates the needed amount of parityBits 'without' the "overall"-Parity
+        HammingSumOfParity = lengthOfDBits => lengthOfDBits == 0 ? 0 : lengthOfDBits < 3 ? lengthOfDBits + 1 :
+            Math.ceil(Math.log2(lengthOfDBits * 2)) <= Math.ceil(Math.log2(1 + lengthOfDBits + Math.ceil(Math.log2(lengthOfDBits)))) ?
+                Math.ceil(Math.log2(lengthOfDBits) + 1) : Math.ceil(Math.log2(lengthOfDBits));
+        const data = value.toString(2).split(""); // first, change into binary string, then create array with 1 bit per index
+        const sumParity = HammingSumOfParity(data.length); // get the sum of needed parity bits (for later use in encoding)
+        const count = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+        // function count for specific entries in the array, for later use
+        const build = ["x", "x", ...data.splice(0, 1)]; // init the "pre-build"
+        for (let i = 2; i < sumParity; i++)
+            build.push("x", ...data.splice(0, Math.pow(2, i) - 1)); // add new paritybits and the corresponding data bits (pre-building array)
+        // Get the index numbers where the parity bits "x" are placed
+        const parityBits = build.map((e, i) => [e, i]).filter(([e, _]) => e == "x").map(([_, i]) => i);
+        for (const index of parityBits) {
+            const tempcount = index + 1; // set the "stepsize" for the parityBit
+            const temparray = []; // temporary array to store the extracted bits
+            const tempdata = [...build]; // only work with a copy of the build
+            while (tempdata[index] !== undefined) {
+                // as long as there are bits on the starting index, do "cut"
+                const temp = tempdata.splice(index, tempcount * 2); // cut stepsize*2 bits, then...
+                temparray.push(...temp.splice(0, tempcount)); // ... cut the result again and keep the first half
+            }
+            temparray.splice(0, 1); // remove first bit, which is the parity one
+            build[index] = (count(temparray, "1") % 2).toString(); // count with remainder of 2 and"toString" to store the parityBit
+        } // parity done, now the "overall"-parity is set
+        build.unshift((count(build, "1") % 2).toString()); // has to be done as last element
+        return build.join(""); // return the build as string
+    },
+},
+{
+    name: 'HammingCodes: Encoded Binary to Integer',
+    solver: function (data) {
+        //check for altered bit and decode
+        const build = data.split(""); // ye, an array for working, again
+        const testArray = []; //for the "truthtable". if any is false, the data has an altered bit, will check for and fix it
+        const sumParity = Math.ceil(Math.log2(data.length)); // sum of parity for later use
+        const count = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+        // the count.... again ;)
+        let overallParity = build.splice(0, 1).join(""); // store first index, for checking in next step and fix the build properly later on
+        testArray.push(overallParity == (count(build, "1") % 2).toString() ? true : false); // first check with the overall parity bit
+        for (let i = 0; i < sumParity; i++) {
+            // for the rest of the remaining parity bits we also "check"
+            const tempIndex = Math.pow(2, i) - 1; // get the parityBits Index
+            const tempStep = tempIndex + 1; // set the stepsize
+            const tempData = [...build]; // get a "copy" of the build-data for working
+            const tempArray = []; // init empty array for "testing"
+            while (tempData[tempIndex] != undefined) {
+                // extract from the copied data until the "starting" index is undefined
+                const temp = [...tempData.splice(tempIndex, tempStep * 2)]; // extract 2*stepsize
+                tempArray.push(...temp.splice(0, tempStep)); // and cut again for keeping first half
+            }
+            const tempParity = tempArray.shift(); // and again save the first index separated for checking with the rest of the data
+            testArray.push(tempParity == (count(tempArray, "1") % 2).toString() ? true : false);
+            // is the tempParity the calculated data? push answer into the 'truthtable'
+        }
+        let fixIndex = 0; // init the "fixing" index and start with 0
+        for (let i = 1; i < sumParity + 1; i++) {
+            // simple binary adding for every boolean in the testArray, starting from 2nd index of it
+            fixIndex += testArray[i] ? 0 : Math.pow(2, i) / 2;
+        }
+        build.unshift(overallParity); // now we need the "overall" parity back in it's place
+        // try fix the actual encoded binary string if there is an error
+        if (fixIndex > 0 && testArray[0] == false) { // if the overall is false and the sum of calculated values is greater equal 0, fix the corresponding hamming-bit           
+            build[fixIndex] = build[fixIndex] == "0" ? "1" : "0";
+        } else if (testArray[0] == false) { // otherwise, if the the overallparity is the only wrong, fix that one           
+            overallParity = overallParity == "0" ? "1" : "0";
+        } else if (testArray[0] == true && testArray.some((truth) => truth == false)) {
+            return 0; // ERROR: There's some strange going on... 2 bits are altered? How? This should not happen
+        }
+        // oof.. halfway through... we fixed an possible altered bit, now "extract" the parity-bits from the build
+        for (let i = sumParity; i >= 0; i--) {
+            // start from the last parity down the 2nd index one
+            build.splice(Math.pow(2, i), 1);
+        }
+        build.splice(0, 1); // remove the overall parity bit and we have our binary value
+        return parseInt(build.join(""), 2); // parse the integer with redux 2 and we're done!
     },
 },
 ]
