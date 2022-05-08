0d807f95435bc7c0905dabb200374576413889b4
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-05-08 20:36:36 -0300

    Run cleanup after pull to avoid red-herring warnings when temp scripts change

--- git-pull.js
+++ git-pull.js
@@ -41,6 +41,8 @@ export async function main(ns) {
     }
     ns.tprint(`INFO: Pull complete. If you have any questions or issues, head over to the Bitburner #alains-scripts Discord channel: ` +
         `https://discord.com/channels/415207508303544321/935667531111342200`);
+    // Remove any temp files / scripts from the prior version
+    ns.run(`${options.subfolder}/cleanup.js`);
 }
 
 /** Joins all arguments as components in a path, e.g. pathJoin("foo", "bar", "/baz") = "foo/bar/baz" **/
