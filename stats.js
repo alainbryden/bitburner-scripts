00261a955c191af0213ebd3ecd18d9f785f14ae6
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-05-10 21:07:57 -0300

    Don't include "home" in purchased servers, and related tidying

--- stats.js
+++ stats.js
@@ -122,15 +122,16 @@ export async function main(ns) {
                 }
             }
 
-            // Show various RAM utilization stats
+            // Show various server / RAM utilization stats
             if (!options['hide-RAM-utilization']) {
                 const servers = await getAllServersInfo(ns);
-                const [count, rooted, purchased] = [servers.length, servers.filter(s => s.hasAdminRights).length, servers.filter(s => s.purchasedByPlayer).length];
+                const rooted = servers.filter(s => s.hasAdminRights).length;
+                const purchased = servers.filter(s => s.hostname != "home" && s.purchasedByPlayer).length; // "home" counts as purchased by the game
                 const likelyHacknet = servers.filter(s => s.hostname.startsWith("hacknet-node-"));
-                // Add Server count
-                addHud("Servers", `${count}/${rooted}/${purchased}`, `The number of servers on the network (${count}) / ` +
-                    `number rooted (${rooted}) / number purchased ` + (9 in dictSourceFiles || 9 == bitNode ?
-                        `(${purchased - likelyHacknet.length} host + ${likelyHacknet.length} hacknet servers)` : `(${purchased})`));
+                // Add Server count.
+                addHud("Servers", `${servers.length}/${rooted}/${purchased}`, `The number of servers on the network (${servers.length}) / ` +
+                    `number rooted (${rooted}) / number purchased ` + (likelyHacknet.length > 0 ?
+                        `(${purchased - likelyHacknet.length} servers + ${likelyHacknet.length} hacknet servers)` : `(${purchased})`));
                 const home = servers.find(s => s.hostname == "home");
                 // Add Home RAM and Utilization
                 addHud("Home RAM", `${ns.nFormat(home.maxRam * 1E9, '0b')} ${(100 * home.ramUsed / home.maxRam).toFixed(1)}%`,
