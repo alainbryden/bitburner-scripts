d150f069c1e05940b521834589b7a7aa446e4689
vvignaux <54744293+vvignaux@users.noreply.github.com>
2022-05-22 14:00:50 +0200

    Added missing contract types solvers (Compression I / II / III) (#148)
    * Added missing contract types solvers
    
    * Fixed solver name capitalization for proper lookup

--- Tasks/contractor.js.solver.js
+++ Tasks/contractor.js.solver.js
@@ -379,7 +379,7 @@ const codingContractTypesMetadata = [{
 {
     name: 'Shortest Path in a Grid',
     solver: function (data) {
-        //slightly adapted and simplified to get rid of MinHeap usage, and construct a valid path from poential candidates   
+        //slightly adapted and simplified to get rid of MinHeap usage, and construct a valid path from potential candidates   
         //MinHeap replaced by simple array acting as queue (breadth first search)  
         const width = data[0].length;
         const height = data.length;
@@ -435,10 +435,9 @@ const codingContractTypesMetadata = [{
         while (queue.length > 0) {
             const [y, x] = queue.shift()
             for (const [yN, xN] of neighbors(y, x)) {
-                const d = distance[y][x] + 1
                 if (distance[yN][xN] == Infinity){
-                    queue.push([yN, xN]);
-                    distance[yN][xN] = d;
+                    queue.push([yN, xN])
+                    distance[yN][xN] = distance[y][x] + 1
                 }
             }
         }
@@ -446,7 +445,7 @@ const codingContractTypesMetadata = [{
         // No path at all?
         if (distance[dstY][dstX] == Infinity) return "";
 
-        //trace path back to start
+        //trace a path back to start
         let path = ""
         let [yC, xC] = [dstY, dstX]
         while (xC != 0 || yC != 0){
@@ -544,7 +543,7 @@ const codingContractTypesMetadata = [{
 },
 {
     //Taken from https://github.com/danielyxie/bitburner/blob/dev/src/utils/HammingCodeTools.ts and converted to js by Discord: H3draut3r#6722
-    name: 'HammingCodes: Integer to encoded Binary',
+    name: 'HammingCodes: Integer to Encoded Binary',
     solver: function (value) {
         // Calculates the needed amount of parityBits 'without' the "overall"-Parity
         const HammingSumOfParity = lengthOfDBits => lengthOfDBits == 0 ? 0 : lengthOfDBits < 3 ? lengthOfDBits + 1 :
@@ -664,4 +663,175 @@ const codingContractTypesMetadata = [{
         return solution
     },
 },
+{
+    name: "Compression I: RLE Compression",
+    solver: function (data) {
+        //original code doesn't generate an answer, but validates it, fallback to this one-liner
+        return data.replace(/([\w])\1{0,8}/g, (group, chr) => group.length + chr)
+    }
+},
+{
+    name: "Compression II: LZ Decompression",
+    solver: function (compr) {
+        let plain = "";
+
+        for (let i = 0; i < compr.length; ) {
+          const literal_length = compr.charCodeAt(i) - 0x30;
+      
+          if (literal_length < 0 || literal_length > 9 || i + 1 + literal_length > compr.length) {
+            return null;
+          }
+      
+          plain += compr.substring(i + 1, i + 1 + literal_length);
+          i += 1 + literal_length;
+      
+          if (i >= compr.length) {
+            break;
+          }
+          const backref_length = compr.charCodeAt(i) - 0x30;
+      
+          if (backref_length < 0 || backref_length > 9) {
+            return null;
+          } else if (backref_length === 0) {
+            ++i;
+          } else {
+            if (i + 1 >= compr.length) {
+              return null;
+            }
+      
+            const backref_offset = compr.charCodeAt(i + 1) - 0x30;
+            if ((backref_length > 0 && (backref_offset < 1 || backref_offset > 9)) || backref_offset > plain.length) {
+              return null;
+            }
+      
+            for (let j = 0; j < backref_length; ++j) {
+              plain += plain[plain.length - backref_offset];
+            }
+
+            i += 2;
+          }
+        }
+
+        return plain;
+    }
+},
+{
+    name: "Compression III: LZ Compression",
+    solver: function (plain) {
+        let cur_state = Array.from(Array(10), () => Array(10).fill(null));
+        let new_state = Array.from(Array(10), () => Array(10));
+
+        function set(state, i, j, str) {
+            const current = state[i][j];
+            if (current == null || str.length < current.length) {
+                state[i][j] = str;
+            } else if (str.length === current.length && Math.random() < 0.5) {
+                // if two strings are the same length, pick randomly so that
+                // we generate more possible inputs to Compression II
+                state[i][j] = str;
+            }
+        }
+
+        // initial state is a literal of length 1
+        cur_state[0][1] = "";
+
+        for (let i = 1; i < plain.length; ++i) {
+            for (const row of new_state) {
+                row.fill(null);
+            }
+            const c = plain[i];
+
+            // handle literals
+            for (let length = 1; length <= 9; ++length) {
+                const string = cur_state[0][length];
+                if (string == null) {
+                    continue;
+                }
+
+                if (length < 9) {
+                    // extend current literal
+                    set(new_state, 0, length + 1, string);
+                } else {
+                    // start new literal
+                    set(new_state, 0, 1, string + "9" + plain.substring(i - 9, i) + "0");
+                }
+
+                for (let offset = 1; offset <= Math.min(9, i); ++offset) {
+                    if (plain[i - offset] === c) {
+                        // start new backreference
+                        set(new_state, offset, 1, string + length + plain.substring(i - length, i));
+                    }
+                }
+            }
+
+            // handle backreferences
+            for (let offset = 1; offset <= 9; ++offset) {
+                for (let length = 1; length <= 9; ++length) {
+                    const string = cur_state[offset][length];
+                    if (string == null) {
+                        continue;
+                    }
+
+                    if (plain[i - offset] === c) {
+                        if (length < 9) {
+                            // extend current backreference
+                            set(new_state, offset, length + 1, string);
+                        } else {
+                            // start new backreference
+                            set(new_state, offset, 1, string + "9" + offset + "0");
+                        }
+                    }
+
+                    // start new literal
+                    set(new_state, 0, 1, string + length + offset);
+
+                    // end current backreference and start new backreference
+                    for (let new_offset = 1; new_offset <= Math.min(9, i); ++new_offset) {
+                        if (plain[i - new_offset] === c) {
+                            set(new_state, new_offset, 1, string + length + offset + "0");
+                        }
+                    }
+                }
+            }
+
+            const tmp_state = new_state;
+            new_state = cur_state;
+            cur_state = tmp_state;
+        }
+
+        let result = null;
+
+        for (let len = 1; len <= 9; ++len) {
+            let string = cur_state[0][len];
+            if (string == null) {
+                continue;
+            }
+
+            string += len + plain.substring(plain.length - len, plain.length);
+            if (result == null || string.length < result.length) {
+                result = string;
+            } else if (string.length == result.length && Math.random() < 0.5) {
+                result = string;
+            }
+        }
+
+        for (let offset = 1; offset <= 9; ++offset) {
+            for (let len = 1; len <= 9; ++len) {
+                let string = cur_state[offset][len];
+                if (string == null) {
+                    continue;
+                }
+
+                string += len + "" + offset;
+                if (result == null || string.length < result.length) {
+                    result = string;
+                } else if (string.length == result.length && Math.random() < 0.5) {
+                    result = string;
+                }
+            }
+        }
+
+        return result ?? "";
+    }
+}
 ]
