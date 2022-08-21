1500ef388cf5772f698985076896301a170230b1
xtrapwr <xtrapwr@gmail.com>
2022-08-21 09:16:26 -0400

    Removed .location check

--- sleeve.js
+++ sleeve.js
@@ -202,8 +202,8 @@ async function pickSleeveTask(ns, playerInfo, i, sleeve, canTrain) {
             /*5*/["Diplomacy"], /*6*/["Field Analysis"], /*7*/["Recruitment"]];
         let [action, contractName] = bladeburnerCityChaos > 50 ? ["Diplomacy"] : bbTasks[i];
         // If the sleeve is performing an action with a chance of failure, fallback to another task
-        if (sleeve.location.includes("%") && !sleeve.location.includes("100%"))
-            bladeburnerTaskFailed[i] = Date.now(); // If not, don't re-attempt this assignment for a while
+        //if (sleeve.location.includes("%") && !sleeve.location.includes("100%"))
+        //    bladeburnerTaskFailed[i] = Date.now(); // If not, don't re-attempt this assignment for a while
         // As current city chaos gets progressively bad, assign more and more sleeves to Diplomacy to help get it under control
         if (bladeburnerCityChaos > (10 - i) * 10) // Later sleeves are first to get assigned, sleeve 0 is last at 100 chaos.
             [action, contractName] = ["Diplomacy"]; // Fall-back to something long-term useful
