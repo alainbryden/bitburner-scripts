76040142f9c10327e2d6d25fdc93c3870354a37e
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-08-29 12:03:58 -0300

    Improved support for complex statements and printing results

--- run-command.js
+++ run-command.js
@@ -18,7 +18,20 @@ export async function main(ns) {
     const firstArg = String(args[0]);
     const escaped = escapeChars.some(c => firstArg.startsWith(c) && firstArg.endsWith(c));
     let command = args == escaped ? args[0] : args.join(" "); // If args weren't escaped, join them together
-    //3.6 return await runCommand(ns.run, ns.write, command, `/Temp/terminal-command.js`, !silent);
+    // To avoid confusion, strip out any trailing spaces / semicolons
+    command = command.trim();
+    if (command.endsWith(';')) command = command.slice(0, -1);
+    // If the command appears to contian multiple statements, cleverly (and perhaps perilously)
+    // see if we can inject a return statement so that we can get the return value of the last statement
+    if (command.includes(';')) {
+        const lastStatement = command.lastIndexOf(';');
+        if (!command.slice(lastStatement + 1).trim().startsWith('return'))
+            command = command.slice(0, lastStatement + 1) + `return ` + command.slice(lastStatement + 1);
+        // Create a scope around multi-statement commands so they can be used in a lambda
+        command = `{ ${command} }`;
+    }
+    // Wrapping the command in a lambda that can capture and print its output.
+    command = `ns.tprint(JSON.stringify((() => ${command})() ?? "(no output)", null, 2))`;
     await ns.write(`/Temp/terminal-command.js`, "", "w"); // Clear the previous command file to avoid a warning about re-using temp script names. This is the one exception.
     return await runCommand(ns, command, `/Temp/terminal-command.js`, (escaped ? args.slice(1) : undefined), !silent);
 }
