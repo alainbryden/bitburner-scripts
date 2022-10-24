9d2c798bb1e148505b6c08843c359c9f0ade4440
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2022-10-23 22:45:00 -0300

    New gang members are "free" - fix long standing bug where I assumed they cost reputation

--- gangs.js
+++ gangs.js
@@ -228,7 +228,7 @@ async function onTerritoryTick(ns, myGangInfo) {
     // Update gang members in case someone died in a clash
     myGangMembers = await getNsDataThroughFile(ns, 'ns.gang.getMemberNames()', '/Temp/gang-member-names.txt');
     const nextMemberCost = Math.pow(5, myGangMembers.length - (3 /*numFreeMembers*/ - 1));
-    if (myGangMembers.length < 12 /* Game Max */ && myGangInfo.respect * 0.75 > nextMemberCost) // Don't spend more than 75% of our respect on new members.
+    if (myGangMembers.length < 12 /* Game Max */ && myGangInfo.respect >= nextMemberCost)
         await doRecruitMember(ns) // Recruit new members if available
     const dictMembers = await getGangInfoDict(ns, myGangMembers, 'getMemberInformation');
     if (!options['no-auto-ascending']) await tryAscendMembers(ns); // Ascend members if we deem it a good time
