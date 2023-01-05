265592208cc1da3209131be14fa3f776f357620c
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2023-01-05 12:42:40 -0400

    Clarify error message

--- bladeburner.js
+++ bladeburner.js
@@ -56,7 +56,7 @@ export async function main(ns) {
     //if (!(6 in ownedSourceFiles) && player.bitNodeN != 7) // NOTE: Despite the SF6 description, it seems you don't need SF6
     //    return log(ns, "ERROR: You have not yet unlocked bladeburner outside of BNs 6 & 7 (need SF6)", true, 'error');
     if (!(7 in ownedSourceFiles))
-        return log(ns, "ERROR: You have not yet unlocked the bladeburner API (need SF7)", true, 'error');
+        return log(ns, "ERROR: You have not yet unlocked the bladeburner API (need SF7 or to be in BN7)", true, 'error');
     if (player.bitNodeN == 8)
         return log(ns, "ERROR: Bladeburner is completely disabled in Bitnode 8 :`(\nHappy stonking", true, 'error');
     // Ensure we've joined bladeburners before proceeding further
