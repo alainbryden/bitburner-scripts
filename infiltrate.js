1f20252e17ab90967de77152f1d31393e550dc7c
Arthan-the-one <5609781+Arthan-the-one@users.noreply.github.com>
2023-04-22 17:58:39 +0200

    Fixed guard attack minigame

--- infiltrate.js
+++ infiltrate.js
@@ -108,7 +108,7 @@ const infiltrationGames = [
     },
   },
   {
-    name: "slash when his guard is down",
+    name: "attack when his guard is down",
     init: function (screen) {
       state.game.data = "wait";
     },
@@ -122,7 +122,7 @@ const infiltrationGames = [
 
       // Attack in next frame - instant attack sometimes
       // ends in failure.
-      if ("wait" === state.game.data && -1 !== data.indexOf("ATTACKING!")) {
+      if ("wait" === state.game.data && -1 !== data.indexOf("Preparing?")) {
         state.game.data = "attack";
       }
     },
