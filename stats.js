98218e77bc904b2e3c7e68b5eb0471a8b3083be4
Arthan <5609781+Arthan-the-one@users.noreply.github.com>
2023-05-30 13:18:35 +0200

    added start-stop-button to overview

--- stats.js
+++ stats.js
@@ -19,7 +19,10 @@ const doc = eval('document'),
     hook1 = doc.getElementById('overview-extra-hook-1')
 
 let playerInBladeburner = false,
-    nodeMap = {}
+    nodeMap = {},
+    fullWidthElements,
+    infiltrationChanged=false,
+    stopInfiltration=false
 
 function prepareHudElements(hudData) {
     const newline = (id, txt, toolTip = "") => {
@@ -252,6 +255,56 @@ async function getHudData(ns, bitNode, dictSourceFiles, options) {
 }
 
 /** @param {NS} ns **/
+function prepareFullWidthHud(ns) {
+    // preparing a full-width entry to keep buttons etc.
+    {
+        const above = hook0.parentElement.parentElement,
+        newNode = above.cloneNode(true)
+
+        newNode.firstChild.remove()
+        newNode.firstChild.remove()
+        fullWidthElements = newNode.firstChild.firstChild
+        fullWidthElements.id="overview-extra-hook-custom"
+        above.insertAdjacentElement("afterend", newNode)
+        fullWidthElements.parentElement.colSpan=2
+        fullWidthElements.parentElement.style["text-align"]="center"
+    }
+
+    const addButton = (hook, text, doc, callback) => {
+        let element = doc.createElement("button")
+        // this will not last, find alternative
+        element.className = "css-fw8wf6"
+//        element.style = ""
+        element.textContent=text
+        element.type="button"
+        hook.appendChild(element)
+        element.addEventListener('click', callback)
+        return element
+    }
+
+    //infiltration start/stop button. callbacks cannot use ns, because they cannot be awaited
+    {
+        stopInfiltration = ns.read("/Temp/stopInfiltration.txt")
+        let element
+        const buttonCallback = () => {
+            infiltrationChanged=true
+            stopInfiltration=!stopInfiltration
+            element.textContent=(stopInfiltration ? "start" : "stop") + " infiltrating"
+        }
+        element = addButton(fullWidthElements, (stopInfiltration ? "start" : "stop") + " infiltrating", doc, buttonCallback)
+    }
+}
+
+/** @param {NS} ns **/
+function updateFullWidthHud(ns) {
+    if (infiltrationChanged) {
+        infiltrationChanged = false
+        if (stopInfiltration) ns.write("/Temp/stopInfiltration.txt", "true")
+        else ns.rm("/Temp/stopInfiltration.txt")
+    }
+}
+
+/** @param {NS} ns **/
 export async function main(ns) {
     const options = getConfiguration(ns, argsSchema);
     if (!options || await instanceCount(ns) > 1) return; // Prevent multiple instances of this script from being started, even with different args.
@@ -262,11 +315,15 @@ export async function main(ns) {
     disableLogs(ns, ['sleep']);
 
     // Hook script exit to clean up after ourselves.
-    ns.atExit(() => hook1.innerHTML = hook0.innerHTML = "")
+    ns.atExit(() => {
+        hook1.innerHTML = hook0.innerHTML = ""
+        fullWidthElements.parentElement.parentElement.remove()
+    })
 
     addCSS(doc);
 
     prepareHudElements(await getHudData(ns, bitNode, dictSourceFiles, options))
+    prepareFullWidthHud(ns)
 
     // Main stats update loop
     while (true) {
@@ -277,6 +334,7 @@ export async function main(ns) {
             for (const [header, show, formattedValue, toolTip] of hudData) {
                 updateHudElement(header, show, formattedValue, toolTip)
             }
+            updateFullWidthHud(ns)
         } catch (err) {
             // Might run out of ram from time to time, since we use it dynamically
             log(ns, `WARNING: stats.js Caught (and suppressed) an unexpected error in the main loop. Update Skipped:\n` +
