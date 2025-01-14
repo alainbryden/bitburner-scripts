a02efac396c3f08f468c982dfbc6cff1980cc1ec
Alain Bryden <2285037+alainbryden@users.noreply.github.com>
2025-01-14 09:58:41 -0400

    In bladeburner, stanek should should prioritize the bladeburner piece
    Luckily, by default it prioritizes using the hack pieces (0 and 1) - and 0 is the same shape as 30 (bladeburner), so we can just look for 0 and swap it with 30.

--- stanek.js.create.js
+++ stanek.js.create.js
@@ -37,6 +37,17 @@ export async function main(ns) {
         log(ns, 'Cleared any existing stanek layout.');
     }
 
+    // If we're in a bladeburner BN, and there's no bladeburner piece in our selected layout (id=30),
+    // replace the identically shaped hacking multi piece  with this one.
+    const has0butNot30 = bestLayout.fragments.some(p => p.id == 0) && !bestLayout.fragments.some(p => p.id == 30);
+    if (has0butNot30) {
+        const bitnodeN = (await getNsDataThroughFile(ns, 'ns.getResetInfo()')).currentNode;;
+        if (bitnodeN == 6 || bitnodeN == 7) {
+            log(ns, `We're in a bladeburner node, replacing a hack piece with the bladeburner piece.`);
+            bestLayout.fragments.find(p => p.id == 0).id = 30;
+        }
+    }
+
     // Place the layout
     log(ns, `Placing ${bestLayout.fragments.length} fragments:\n` + JSON.stringify(bestLayout.fragments));
     const result = await getNsDataThroughFile(ns,
